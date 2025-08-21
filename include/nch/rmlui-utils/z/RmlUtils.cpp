ab4348f5f6c794e8868a46b16c643ed552c74f5d
Noah Hebert <noah.c.h@outlook.com>
2025-08-21 13:27:29 -0400

    Hotfix 2

--- include/nch/rmlui-utils/z/RmlUtils.cpp
+++ include/nch/rmlui-utils/z/RmlUtils.cpp
@@ -5,8 +5,8 @@ Box2f RmlUtils::getElementBox(Rml::Element* elem)
 {
     if(elem==nullptr) return Box2f(-1,-1,-1,-1);
     return Box2f(
-        elem->GetAbsoluteLeft,
-        elem->GetAbsoluteTop,
+        elem->GetAbsoluteLeft(),
+        elem->GetAbsoluteTop(),
         elem->GetBox().GetSize().x,
                  elem->GetBox().GetSize().y,
     );
